c15993c971dd54d3cef481b0fec3799476a1d184
winadiw <winadiw@gmail.com>
2016-07-28 19:58:23 +0700

    Delete file
    file lama

--- Code/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Lecture02Parabolic lecture02 = new Lecture02Parabolic();
-                //Tambah Comment Buat ngecek bener gak diupdate di src
-	}
-	
-}
